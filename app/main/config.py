53abdb9a34924edd66686e0e8d8f2f51d2c75ce3
AntoineDao <antoinedao1@gmail.com>
2018-10-13 12:51:12 +0100

    Updated Postgres config

--- app/main/config.py
+++ app/main/config.py
@@ -1,12 +1,25 @@
 import os
+from dotenv import load_dotenv
+load_dotenv()
 
 # uncomment the line below for postgres database url from environment variable
 # postgres_local_base = os.environ['DATABASE_URL']
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
-
 class Config:
+    postgres_address = os.getenv('PGADDRESS')
+    postgres_port = os.getenv('PGPORT')
+    postgres_user = os.getenv('PGUSER')
+    postgres_password = os.getenv('PGPASS')
+
+    postgres_local_base =\
+        "postgresql://{user}:{password}@{address}:{port}"\
+        .format(user=postgres_user,
+                password=postgres_password,
+                address=postgres_address,
+                port=postgres_port)
+
     SECRET_KEY = os.getenv('SECRET_KEY', 'my_precious_secret_key')
     DEBUG = False
 
@@ -17,8 +30,7 @@ class DevelopmentConfig(Config):
     # uncomment the line below to use postgres
     # SQLALCHEMY_DATABASE_URI = postgres_local_base
     DEBUG = True
-    SQLALCHEMY_DATABASE_URI =\
-        "postgresql://postgres@/{db_name}"\
+    SQLALCHEMY_DATABASE_URI = Config.postgres_local_base + '/{db_name}'\
         .format(db_name=db_name)
 
     SQLALCHEMY_TRACK_MODIFICATIONS = False
@@ -29,12 +41,8 @@ class TestingConfig(Config):
 
     DEBUG = True
     TESTING = True
-    # SQLALCHEMY_SERVER_URI =\
-    #     "postgresql://"\
-    #     .format(user=user, pword=pword, server_ip=server_ip, port=port)
 
-    SQLALCHEMY_DATABASE_URI =\
-        "postgresql://postgres@/{db_name}"\
+    SQLALCHEMY_DATABASE_URI = Config.postgres_local_base + '/{db_name}'\
         .format(db_name=db_name)
 
     PRESERVE_CONTEXT_ON_EXCEPTION = False
